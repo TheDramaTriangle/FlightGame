64db3c2690c9dedbcbbb11c8b0c5b6ae052d570c
josiah <josiahd.kitchin@gmail.com>
2025-04-18 13:41:34 -0700

    camera follows the plane, third person

--- Assets/Scripts/CameraFollowPlane.cs
+++ Assets/Scripts/CameraFollowPlane.cs
@@ -2,15 +2,14 @@ using UnityEngine;
 
 public class CameraFollowPlane : MonoBehaviour
 {
+    public Transform playerPlane;  
+    public Vector3 offset = new(0, 1, -3); 
+    public float smoothSpeed = 10f; 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+    void LateUpdate()
     {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
+        Vector3 desiredPosition = playerPlane.position + offset; 
+        transform.position = Vector3.Lerp(transform.position, desiredPosition, smoothSpeed);
+        transform.LookAt(playerPlane);
     }
 }
