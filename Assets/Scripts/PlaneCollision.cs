b65c7005684362493b8462e869800308204ad651
josiah <josiahd.kitchin@gmail.com>
2025-04-19 14:46:40 -0700

    added null check in CameraFollowPlane and removed debug log in collision

--- Assets/Scripts/PlaneCollision.cs
+++ Assets/Scripts/PlaneCollision.cs
@@ -5,7 +5,6 @@ public class NewMonoBehaviourScript : MonoBehaviour
 {
     void OnCollisionEnter(Collision collision)
     {
-        Debug.Log("Collision with: " + collision.gameObject.name);
         if (collision.gameObject.CompareTag("Obstacle"))
         {
             Destroy(gameObject);
