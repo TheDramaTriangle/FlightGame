fb80c5cab85d474ced6a13622fe4688b964e30db
aaudie <aaudie@gmail.com>
2025-05-20 15:42:20 -0700

    Axel

--- Assets/AntAnimation.cs
+++ Assets/AntAnimation.cs
@@ -1,80 +1,79 @@
-// using UnityEngine;
-// using System;
-// using System.Collections;
-// using UnityEngine;
-// using UnityEngine.AI;
+using UnityEngine;
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.AI;
 
-// [RequireComponent(typeof(Animator), typeof(NavMeshAgent))]
-// public class AntAnimation : MonoBehaviour
-// {
-//     private Animator        _anim;
-//     private NavMeshAgent    _agent;
-//     private EnemyAttack     _attack;
-//     private EnemyHealth     _health;
-//     private bool            _isDead;
+[RequireComponent(typeof(Animator), typeof(NavMeshAgent))]
+public class AntAnimation : MonoBehaviour
+{
+    private Animator        _anim;
+    private NavMeshAgent    _agent;
+    private EnemyAttack     _attack;
+    private EnemyHealth     _health;
+    private bool            _isDead;
 
-//     void Awake()
-//     {
-//         _anim  = GetComponent<Animator>();
-//         _agent = GetComponent<NavMeshAgent>();
-//         _attack = GetComponent<EnemyAttack>();
-//         _health = GetComponent<EnemyHealth>();
-//     }
+    void Awake()
+    {
+        _anim  = GetComponent<Animator>();
+        _agent = GetComponent<NavMeshAgent>();
+        _attack = GetComponent<EnemyAttack>();
+        _health = GetComponent<EnemyHealth>();
+    }
 
-//     void OnEnable()
-//     {
-//         if (_attack != null)
-//             _attack.OnAttack += HandleAttack;
-//         if (_health != null)
-//             _health.OnDeath  += HandleDeath;
-//     }
+    void OnEnable()
+    {
+        if (_attack != null)
+            _attack.OnAttack += HandleAttack;
+        if (_health != null)
+            _health.OnDeath  += HandleDeath;
+    }
 
-//     void OnDisable()
-//     {
-//         if (_attack != null)
-//             _attack.OnAttack -= HandleAttack;
-//         if (_health != null)
-//             _health.OnDeath  -= HandleDeath;
-//     }
+    void OnDisable()
+    {
+        if (_attack != null)
+            _attack.OnAttack -= HandleAttack;
+        if (_health != null)
+            _health.OnDeath  -= HandleDeath;
+    }
 
-//     void Update()
-//     {
-//         if (_isDead) return;
+    void Update()
+    {
+        if (_isDead) return;
 
-//         // Walking animation
-//         bool walking = _agent.velocity.sqrMagnitude > 0.1f;
-//         _anim.SetBool("isWalking", walking);
-//     }
+        // Walking animation
+        bool walking = _agent.velocity.sqrMagnitude > 0.1f;
+        _anim.SetBool("isWalking", walking);
+    }
 
-//     public void HandleAttack()
-//     {
-//         // Attack animation
-//         Debug.Log ("Ant animation");
-//         _anim.SetTrigger("Attack");
-//     }
+    public void HandleAttack()
+    {
+        // Attack animation
+        _anim.SetTrigger("Attack");
+    }
 
-//     private void HandleDeath()
-//     {
-//         _isDead = true;
-//         _anim.SetTrigger("Die");
+    private void HandleDeath()
+    {
+        _isDead = true;
+        _anim.SetTrigger("Die");
 
-//         // stop moving/collisions
-//         _agent.isStopped = true;
-//         var col = GetComponent<Collider>();
-//         if (col) col.enabled = false;
+        // stop moving/collisions
+        _agent.isStopped = true;
+        var col = GetComponent<Collider>();
+        if (col) col.enabled = false;
 
-//         // queue destroy after death clip length
-//         float delay = GetDeathClipLength();
-//         Destroy(gameObject, delay);
-//     }
+        // queue destroy after death clip length
+        float delay = GetDeathClipLength();
+        Destroy(gameObject, delay);
+    }
 
-//     private float GetDeathClipLength()
-//     {
-//         // find the clip named "Death" on the runtime controller
-//         foreach (var clip in _anim.runtimeAnimatorController.animationClips)
-//             if (clip.name == "Death")
-//                 return clip.length;
-//         // fallback
-//         return 1f;
-//     }
-// }
+    private float GetDeathClipLength()
+    {
+        // find the clip named "Death" on the runtime controller
+        foreach (var clip in _anim.runtimeAnimatorController.animationClips)
+            if (clip.name == "Death")
+                return clip.length;
+        // fallback
+        return 1f;
+    }
+}
--- Assets/Scripts/Enemy/EnemyAttack.cs
+++ Assets/Scripts/Enemy/EnemyAttack.cs
@@ -1,64 +1,72 @@
 
 
-using UnityEngine;
-
-/// <summary>
-/// If the enemy is touching the defense object, deal damageAmount to the defense object
-/// at a set time interval 
-/// </summary>
-public class EnemyAttack : MonoBehaviour
-{
-    public float attackCoolDown = 1f;  
-    public int damageAmount = 10; 
-    private GameObject defenseTarget = null; 
-    private float lastAttackTime = 0f;
-
-    private void OnCollisionStay(Collision collision)
-    {
-        if (Time.time - lastAttackTime < attackCoolDown) return;
-
-        if (collision.gameObject.CompareTag("Defense"))
-        {
-            defenseTarget = collision.gameObject; 
-            defenseTarget.GetComponent<DefenseHealth>().TakeDamage(damageAmount);
-            lastAttackTime = Time.time; 
-        }
-
-    }
-}
-// using System;
 // using UnityEngine;
 
+// /// <summary>
+// /// If the enemy is touching the defense object, deal damageAmount to the defense object
+// /// at a set time interval 
+// /// </summary>
 // public class EnemyAttack : MonoBehaviour
 // {
-//     public float attackCoolDown = 1f;
-//     public int   damageAmount   = 10;
-//     public AntAnimation animation;
-
+//     public float attackCoolDown = 1f;  
+//     public int damageAmount = 10; 
+//     private GameObject defenseTarget = null; 
 //     private float lastAttackTime = 0f;
-//     public event Action OnAttack;
-//     public Collider DetectSphere;
 
 //     private void OnCollisionStay(Collision collision)
 //     {
-//         if (Time.time - lastAttackTime < attackCoolDown) 
-//             return;
+//         if (Time.time - lastAttackTime < attackCoolDown) return;
 
 //         if (collision.gameObject.CompareTag("Defense"))
 //         {
-//             // fire animation event
-//             Debug.Log (name+"~ Trying to attack");
-//             OnAttack?.Invoke();
-//             animation.HandleAttack();
-
-//             // deal damage
-//             collision.gameObject.GetComponent<DefenseHealth>()
-//                       .TakeDamage(damageAmount);
-//             lastAttackTime = Time.time;
+//             defenseTarget = collision.gameObject; 
+//             defenseTarget.GetComponent<DefenseHealth>().TakeDamage(damageAmount);
+//             lastAttackTime = Time.time; 
 //         }
-//     }
 
-//     private void Start (){
-//         animation = GetComponent<AntAnimation>();
 //     }
 // }
+using System;
+using UnityEngine;
+
+public class EnemyAttack : MonoBehaviour
+{
+    public float attackCoolDown = 1f;
+    public int   damageAmount   = 10;
+    public AntAnimation animation;
+
+    private float lastAttackTime = 0f;
+    public event Action OnAttack;
+    public UnityEngine.AI.NavMeshAgent NavAgent;
+    public float tolerance = 2f; //distance at which the attacking animation trigers
+    public EnemyChaser targetFinder;
+
+    private void Update()
+    {
+        if (NavAgent.remainingDistance < tolerance)
+        {
+            NavAgent.isStopped = true;
+            lastAttackTime += Time.deltaTime;
+            if (attackCoolDown - lastAttackTime < 0f)
+            {
+                // fire animation event
+                lastAttackTime = 0.0f;
+                OnAttack?.Invoke();
+                animation.HandleAttack();
+                // deal damage
+                targetFinder.target.gameObject.GetComponent<DefenseHealth>()
+                        .TakeDamage(damageAmount);
+            }
+        }
+        else
+        {
+            NavAgent.isStopped = false;
+        }
+    }
+
+    private void Start (){
+        NavAgent = GetComponent<UnityEngine.AI.NavMeshAgent>();
+        targetFinder = GetComponent<EnemyChaser>();
+        animation = GetComponent<AntAnimation>();
+    }
+}
