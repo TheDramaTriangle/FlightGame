4a95f3c1df27dbdf7048de0567a5c44dc51be5c0
josiah <josiahd.kitchin@gmail.com>
2025-05-28 10:53:19 -0700

    added a health bar

--- Assets/Scripts/Enemy/EnemyHealth.cs
+++ Assets/Scripts/Enemy/EnemyHealth.cs
@@ -17,19 +17,29 @@
 
 using System;
 using UnityEngine;
+using UnityEngine.UI; 
 
 public class EnemyHealth : MonoBehaviour
 {
-    public int health = 100;
+    const int maxHealth = 100; 
+    public int health;
     public event Action OnDeath;
+    public Image healthBar; 
 
     // optional: set this in inspector if you don't want to
     // hard-code the death clip length
     public float deathDelay = 1f; 
 
+    public void Start()
+    {
+        health = maxHealth; 
+        updateHealthBar();
+    }
+
     public void TakeDamage(int damageAmount)
     {
         health -= damageAmount;
+        updateHealthBar(); 
         if (health <= 0)
         {
             // notify animation controller
@@ -38,5 +48,11 @@ public class EnemyHealth : MonoBehaviour
             // delay actual destruction to let the death animation play
             Destroy(gameObject, deathDelay);
         }
+    }
+
+    public void updateHealthBar()
+    {
+        if (healthBar != null)  
+            healthBar.fillAmount = (float)health / (float)maxHealth;
     }
 }
